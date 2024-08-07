ffd2703d1dbb689e3f598850d5b58f3a06a8b5de
sovitha <khadkabinisha48@gmail.com>
2024-08-07 17:29:43 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hello world")
+const shoes =['aaa','bbb','ccc','ddd']
+console.log(shoes[0])
+console.log(shoes[3])
+console.log(shoes.length)
+shoes.push('fast')//at last
+  console.log(shoes)
+shoes.unshift('ast')//at first
+   console.log(shoes)
+ const numbers=[1,2,3,4,5,6]
+   const newNumbers = numbers.slice(0,4)
+   console.log(newNumbers)
+ numbers.splice(2,1,100)
+ console.log(numbers)
+ 
